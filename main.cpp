b44e95c77a0ea917835cc7ea4185814f45ce7b05
kyplyk <avdosev1@yandex.ru>
2019-10-27 18:05:24 +0400

    однÐдвухмерный безье готов

--- main.cpp
+++ main.cpp
@@ -14,13 +14,26 @@
 using vector_points = std::vector<point>;
 
 point bezier(vector_points points, double t) {
+    auto bezier2 = [](point first, point second, double t){
+        return (1-t)*first+t*second;
+    };
+    auto bezier3 = [](point first, point second, point third, double t){
+        return pow((1-t), 2)*first + 2.0*t*(1-t)*second + pow(t, 2)*third;
+    };
+
+    while(points.size() > 3) {
+        for (vector_points::size_type i = 0; i < points.size()-1; i++) {
+            points[i] = bezier2(points[i], points[i+1], t);
+        }
+        points.pop_back();
+    }
+
     if (points.size() == 3) {
-        return pow((1-t), 2)*points[0] + 2.0*t*(1-t)*points[1] + pow(t, 2)*points[2];
+        return bezier3(points[0], points[1], points[2], t);
     } else if (points.size() == 2) {
-        return (1-t)*points[0]+t*points[1];
+        return bezier2(points[0], points[1], t);
     } else {
-        /* чет хз */
-        return point();
+        return points.back();
     }
 }
 
@@ -29,12 +42,13 @@ int main() {
     // получаем набор точек у пользователя
     // по точкам создаем набор точек кривой
     // выводим результат
-    double step = 0.005;
+    double step = 0.05;
     vector_points points{
             {7, 2},
             {5, 7},
             {10, 3}
     };
+    // пока шаг не очень маленький можно так
     for (auto t = 0.0; t <= 1.0; t += step) {
         auto p = bezier(points, t);
         std::cout << "[ " << p.x << ", " << p.y << " ], " << std::endl;
