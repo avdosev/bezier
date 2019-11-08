8ee0f7b03400e86391bb0515e39971049b3ccd05
kyplyk <avdosev1@yandex.ru>
2019-11-08 20:08:02 +0400

    update show bezier

--- show_bezier2d.py
+++ show_bezier2d.py
@@ -4,10 +4,15 @@ with open('bezier2d.json') as file:
     data = json.load(file)
     x = [i[0] for i in data]
     y = [i[1] for i in data]
+    z = [i[2] for i in data]
 
 
     import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
 
-    plt.plot(x,y)
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    ax.plot(x, y, z)
 
     plt.show()
